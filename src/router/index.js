e8212a67a46b128f48700d575714c08d1845baba
Julianna Grefkowicz <cantarellaa00@gmail.com>
2022-07-10 18:16:07 +0200

    Simple add user form added

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import UserList from '../views/UserList.vue'
+import UserCreate from '../views/UserCreate.vue'
+
 
 const routes = [
   {
     path: '/',
     name: 'UserList',
     component: UserList
+  },
+  {
+    path: '/create',
+    name: 'UserCreate',
+    component: UserCreate
   },
 ]
 
---
+++ src/views/UserCreate.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="form-container">
+
+    <form @submit.prevent="onSubmit">
+
+      <label>Name</label>
+      <input
+          v-model="user.name"
+          type="text"
+          placeholder="John Doe"
+      >
+
+      <label>Company</label>
+      <input
+          v-model="user.company"
+          type="text"
+          placeholder="Company Name"
+      />
+
+      <label>E-mail</label>
+      <input
+          v-model="user.email"
+          type="text"
+          placeholder="john@company.com"
+      />
+
+      <label>Quantity</label>
+      <input
+          v-model="user.quantity"
+          type="number"
+          placeholder="1"
+      />
+
+      <label>Price</label>
+      <input
+          v-model="user.price"
+          type="number"
+          placeholder="100"
+      />
+
+      <button type="submit">Submit</button>
+    </form>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      user: {
+        id: 0,
+        name: '',
+        company: '',
+        email: '',
+        quantity: 0,
+        price: 0
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      console.log("User:", this.user)
+    }
+  }
+}
+</script>
