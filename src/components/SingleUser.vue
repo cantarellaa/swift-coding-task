0df3a1654291041b4b9541ca38b147e50eead228
Julianna Grefkowicz <cantarellaa00@gmail.com>
2022-07-10 16:40:38 +0200

    Cleaned up the default environment

---
+++ src/components/SingleUser.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="single-user">
+    <div> {{ user.id }}</div>
+    <div> {{ user.name }}</div>
+    <div> {{ user.company }}</div>
+    <div> {{ user.email }}</div>
+    <div> {{ user.quantity }}</div>
+    <div> {{ user.price }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SingleUser',
+  data() {
+    return {
+      user: {
+        id: 1,
+        name: 'John Doe',
+        company: 'Company Name',
+        email: 'john@company.com',
+        quantity: '5',
+        price: '49.90'
+      }
+    }
+  }
+  // props: {
+  //   msg: String
+  // }
+
+}
+</script>
+
+<style scoped>
+  .single-user {
+    padding: 20px;
+    width: 250px;
+    border: 1px solid #39495c;
+    margin-bottom: 18px;
+  }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SingleUser />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import SingleUser from '@/components/SingleUser.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    SingleUser
   }
 }
 </script>
